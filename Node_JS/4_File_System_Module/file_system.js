5f82e48927f9d10f72fe23c66350ff5009800aaa
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-26 22:54:11 +0500

    File_System_folder and files.txt created  02/26/2025

---
+++ Node_JS/4_File_System_Module/file_system.js
@@ -0,0 +1,8 @@
+// File System Module:
+    // This is third one built-in module. It is use for read and write the data from
+    // extension files such as .txt and others.
+// Let's  Observ and write the basic coding to read and write the contents in files.
+
+// Create the a viarables for file system as  readFileSync and writeFileSync
+
+
