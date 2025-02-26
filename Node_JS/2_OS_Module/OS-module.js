51285913de2d9244c231c264ed8545ebbf3e950c
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-26 15:00:36 +0500

    platform() udded 02/26/2025

--- Node_JS/2_OS_Module/OS-module.js
+++ Node_JS/2_OS_Module/OS-module.js
@@ -34,7 +34,7 @@ console.log('The current type of Operating System is ' + OS.type());
 console.log('Current CPU architecture is ' + OS.arch());
 // arch() is return the CPU architecture.
 
-console.log('The total ammount of memory is ' + OS.totalmem() + ' byte');
+console.log('The total ammount of memory is ' + OS.totalmem() + ' byte Ram');
 // Another OS method totalmem() is used to return the total memory of System.
 // totalmem return the memory in byte so if we want to convert it in another form so we do under
 
@@ -42,6 +42,10 @@ let kiloByte = parseInt(OS.totalmem())/(1024); //return kilobytes
 let megaByte = parseInt(OS.totalmem())/(1024*1024); // return megabytes
 let gigaByte = parseInt(OS.totalmem())/(1024*1024*1024); // return gigabytes
 // and so on....
-console.log('Total memory in kilobyte is ' + Math.ceil(kiloByte) + ' KB');
-console.log('Total memory in megabyte is ' + Math.ceil(megaByte) + ' MB');
-console.log('Total memory in gigabyte is ' + Math.ceil(gigaByte) + ' GB');
+console.log('Total memory in kilobyte is ' + Math.ceil(kiloByte) + ' KB Ram');
+console.log('Total memory in megabyte is ' + Math.ceil(megaByte) + ' MB Ram');
+console.log('Total memory in gigabyte is ' + Math.ceil(gigaByte) + ' GB Ram');
+
+
+//Let's do more practices with other OS methods. So we can understand meaning of OS.
+
