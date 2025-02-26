43f3d2bca2b62b6da582532882756e3c368e8b2e
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-26 17:04:46 +0500

    Path_module created and js file added  02/26/2025

---
+++ Node_JS/3_Path_Module/path_module.js
@@ -0,0 +1,8 @@
+
+// create path module object
+var pathObject = require('path');
+
+// just print the some data by using its methods
+
+console.log(pathObject.sep);
+// when we run above code line it will show only backward slash
