e4f8be7a33c93993b97c464943d12eb3e7063dbc
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-26 14:24:40 +0500

    OS definition added 02/26/2025

---
+++ Node_JS/2_OS_Module/OS-module.js
@@ -0,0 +1,22 @@
+/*
+// OS Module    
+    Before we start code first need to understand the basic concept of OS module.
+    OS stand for Operating System module is the predefined or built-in module in the Node.js framework.
+    Now question is arrived in mind why we use the OS module and how to implement it
+    with code.
+    Official Definition:
+        The os module in Node.js is a built-in module that provides utilities for interacting with the 
+        operating system. It allows developers to retrieve system-related information such as CPU 
+        architecture, memory usage, network interfaces, and more.
+
+    And of course as the definition it is built-in module it means it has predefined 
+    methods such as:
+        type()
+        platform()
+        arch()
+        totalmem()
+        etc..
+    We will discuss each during the coding.
+    So let's begin:)------>>>>>>>>>>>>>>>>>>>>>       
+
+*/
