9b87e1110e56279a5e8dabe6103a8948151d6c28
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-26 23:29:11 +0500

    File_System_fs_Methods_added  02/26/2025

--- Node_JS/4_File_System_Module/file_system.js
+++ Node_JS/4_File_System_Module/file_system.js
@@ -23,20 +23,24 @@ console.log("The following contents are created from fs write method");
 console.log(data3);
 
 // Another way to write the data into one file to another file.
-writeFileSync('./data/onlyWrite.txt', `This is data from aboutMe file ${data1}`); 
-writeFileSync('./data/onlyWrite.txt', `This is data from skill file ${data2}`);
-writeFileSync('./data/onlyWrite.txt', `This is data from empty file ${data3}`);
-
-// create one instance for onlyWrite file to store all contents from it
-let data4 = readFileSync('./data/onlyWrite.txt', 'utf8');
-
-// Print all contents from onlyWrite File txt extension.
-console.log(data4);
-//Note: If you run above codes in one time to write from three files it will over write 
-// one over to other one So you can write only one time in one file from multiple instances
-// as following:
+writeFileSync('./data/onlyWrite.txt', `This is data from aboutMe file ${data1}`); //over write the contents
+writeFileSync('./data/onlyWrite.txt', `This is data from skill file ${data2}`); //over write the contents
+writeFileSync('./data/onlyWrite.txt', `This is data from empty file ${data3}`); 
+//just this is last one can write the contents into the file
 
 // create one instance for onlyWrite file to store all contents from it
 // let data4 = readFileSync('./data/onlyWrite.txt', 'utf8');
 
+// Print all contents from onlyWrite File txt extension.
+// console.log(data4);
+//Note: If you run above codes in one time to write from three files it will over write 
+// one over to other one So you can write only one time in one file from multiple instances
+// as following:
+
+writeFileSync('./data/onlyWrite.txt', `This is data from all files ${data1} ${data2} ${data3}`);
+// create one instance for onlyWrite file to store all contents from it
+let data5 = readFileSync('./data/onlyWrite.txt', 'utf8');
+console.log(data5);
+
+//Cool it works:)
 
