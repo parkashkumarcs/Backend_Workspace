0a8d0c7fb88618dd1b4a7f715951cd468d0dfdc5
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-27 00:05:50 +0500

    FS_Error_HandlingMehtods  02/26/2025

---
+++ Node_JS/4_File_System_Module/fs_errorHandling.js
@@ -0,0 +1,5 @@
+// As we learnt in previous file_system.js code file where we were just write basic functionality to
+// write and read content from Files. However we can face the errors or exception from files which 
+// can block the other codes. So here can handle these errors by using functions.
+
+// Let's commits first and then start our coding:)
