033bc8b49ba0f247313b939fa33a186b200ce5e3
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-02-28 16:21:23 +0500

    HTTP_file_Created 02/28/2025

--- Node_JS/5_HTTP_Module/http_module.js
+++ Node_JS/5_HTTP_Module/http_module.js
@@ -1,1 +1,17 @@
 // HTTP Module:
+    // HTTP stands for HypterText Transfer Protocol. Which is use to communicat the data on
+    // World Wide Web (wwww) between clients and servers over.
+    // HTTP module is most important module to learn for every developer.
+    // By this learning we set up our codes to communicate our full stack application 
+    // with other servers. 
+// How it works?
+    // Client send a request to server. Resquest could be anything like videos, images, text or queries.
+    // Server executes these requests and send back to clients through HTTP as responses.
+    // And in final DOM render these responses and display the contents as per sent by server to
+    // clients in browsers.
+
+ // Let's explore it through coding:)....
+ 
+ // Create a global variable by using commonJS modules.
+ var myHTTP = require('http');
+ 
